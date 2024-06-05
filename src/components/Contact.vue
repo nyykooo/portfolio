ae0c7d43181d2c89e49ac283f82870abba60d3be
nyykooo <nicholas.cakla@outlook.com>
2024-06-05 22:07:30 +0100

    improving css on project buttons and adding a border to myself

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="contacts">
         <div class="contacts__container">
-            <img src="../assets/image/icon/linkedin.png" class="image_icons"/>
-            <a href="https://www.linkedin.com/in/nicholascampbellcaklamanis/" target="_blank" class="contacts__links">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/nicholascampbellcaklamanis/" target="_blank" class="contacts__links">
+                <img src="../assets/image/icon/linkedin.png" class="image_icons"/> LinkedIn
+            </a>
         </div>
         <div class="contacts__container">
-            <img src="../assets/image/icon/github.png" class="image_icons"/>
-            <a href="https://github.com/nyykooo" target="_blank" class="contacts__links">Github</a>
+            
+            <a href="https://github.com/nyykooo" target="_blank" class="contacts__links">
+                <img src="../assets/image/icon/github.png" class="image_icons"/> Github
+            </a>
         </div>
     </div>
 </template>
